3ebf4cd7fd1c37dd9edbe2ff3a4b4e55109be11a
vegabrandon <vegabrandon1250@gmail.com>
2023-02-20 10:44:13 -0500

    Added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import {motion} from 'framer-motion'
+import ProjectList from './ProjectList';
 
 function App() {
   const navbarLinkClasses = `
   text-white p-2 border-[1px] border-b-white/[.3] border-r-[transparent] border-t-[transparent] border-l-[transparent]
-  hover:background-white hover:cursor-pointer hover:bg-slate-300/[.2] hover:rounded-t-lg ease-in duration-100`
+  hover:background-white hover:cursor-pointer hover:bg-slate-300/[.2] hover:rounded-t-lg ease-in duration-100 text-xl`
   return (
-    <div className="App">
-      <div className="flex flex-row justify-between sticky m-auto w-[100vw] h-[30px] items-center top-5">
+    <div className="App overflow-x-hidden">
+      <div className="flex flex-row justify-between sticky m-auto w-[100vw] h-[30px] items-center top-5  max-w-[100vw]">
         <div className='w-[22vw]'>Logo</div>
         <div className='flex flex-row w-[50vw] justify-evenly'>
           <h3
@@ -25,7 +26,7 @@ function App() {
         </div>
       </div>
       <div className="flex flex-row justify-center mt-[10rem]">
-        <div className='flex flex-col w-[60vw] h-[40vh] text-left justify-evenly'>
+        <div className='flex flex-col w-[60vw] h-[45vh] text-left justify-center'>
           <div>
             <motion.p 
               className='text-white text-2xl'
@@ -35,13 +36,17 @@ function App() {
             >
               Welcome, my name is
             </motion.p>
-            <h1 className='text-white ml-[-17px] text-[10rem]'>Brandon Vega.</h1>
+            <h1 className='text-white ml-[-17px] text-[8rem]'>Brandon Vega.</h1>
           </div>
           
           <p className='text-white text-lg w-[30vw]'>I'm a full stack software engineer specializing in delivering sophisticated and functional web applications. To view some of my work or contact information, click one of the following buttons</p>
         </div>
       </div>
+      <div className='h-[50vh]'></div>
+
+      <ProjectList />
     </div>
+
   );
 }
 
---
+++ src/ProjectList.jsx
@@ -0,0 +1,16 @@
+
+const ProjectList = () => {
+
+    return (
+        <div className='flex flex-col h-[100vh]'>
+            <div>
+                <h1>Projects</h1>
+            </div>
+            <div>
+            
+            </div>
+      </div>
+    )
+}
+
+export default ProjectList;
